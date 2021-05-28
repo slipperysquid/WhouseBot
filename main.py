56ed92639574fd9e73986d5ee1528b6c7191fadc
slipperysquid <coleseto123@gmail.com>
2021-05-27 21:18:02 -0700

    first text action

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+
+#Client init
+client = discord.Client()
+
+#events
+
+@client.event
+async def on_ready():
+    print("I am ready master.")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if (message.content.lower() == "ping"):
+        await message.channel.send("Pong!")
+
+        
+#run client on server
+client.run(os.getenv('TOKEN'))
