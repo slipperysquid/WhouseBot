f79936246292ff416daba578991364dc22375d01
slipperysquid <60982558+slipperysquid@users.noreply.github.com>
2021-05-30 14:41:53 -0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,13 +2,13 @@
 def make_readable_list(message):
     words = []
     word = ""
-    for i in range(0, len(message) - 1):
+    for i in range(0, len(message)):
         if ((ord(message[i]) >= 97 and ord(message[i]) <= 122)):
-            word += message
-            if(i == len(message) - 1):
-                words += word
+            word.append(message[i])
+            if(i == len(message)):
+                words.append(word)
         else:
-            words += word
+            words.append(word)
             word = ""
     
     return words
